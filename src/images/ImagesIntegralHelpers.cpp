81eec6d92d061ffa3d118f9df4f34bc6b3641ee1
Midoritotoro <danya20090504@gmail.com>
2025-01-24 18:18:07 +0300

    Fix

--- src/images/ImagesIntegralHelpers.cpp
+++ src/images/ImagesIntegralHelpers.cpp
@@ -21,6 +21,10 @@ void ExtractByIndices(
     __m128i high_mid_indices = _mm512_extracti32x4_epi32(indices_arr, 2);
     __m128i high_indices = _mm512_extracti32x4_epi32(indices_arr, 3);
 
+  /*  for (int32 i = 0; i < 4; ++i) {
+        std::cout << high_indices.m128i_i32[i] << std::endl;
+    }*/
+
     __m128i low_bytes;
     __m128i low_mid_bytes;
     __m128i high_mid_bytes;
@@ -93,9 +97,11 @@ namespace base::images {
             
             ulong* integralImage = (ulong*)aligned_malloc(width * _height, 64);
 
-            for (int32 x = 0; x < width; x += 16) {
+            int32 x, y;
+            for (x = 0; x < width; ++x) {
+                ulong suma = 0;
                 __m512i sum = _mm512_set1_epi32(0);
-                for (int32 y = 0; y < _height; ++y) {
+                for (y = 0; y + 15 < height; y += 16) {
                     __m512i index = GetVectorized512BitIndex(width, x, y);
                     
                     __m128i tempSum;
@@ -103,19 +109,90 @@ namespace base::images {
 
                     __m256i srcSum = _mm512_castsi512_si256(_mm512_castsi128_si512(tempSum));
 
+                    __m128i low = _mm256_castsi256_si128(srcSum);
+                    __m128i high = _mm256_extracti128_si256(srcSum, 1);
+
+                    __m128i lo_64_01 = _mm_unpacklo_epi32(low, _mm_setzero_si128()); // 1, 0, 2, 0, 3, 0, 4, 0
+                    __m128i hi_64_01 = _mm_unpackhi_epi32(low, _mm_setzero_si128()); // 5, 0, 6, 0, 7, 0, 8, 0
+
+                    __m128i lo_64_23 = _mm_unpacklo_epi32(high, _mm_setzero_si128()); // 1, 0, 2, 0, 3, 0, 4, 0
+                    __m128i hi_64_23 = _mm_unpackhi_epi32(high, _mm_setzero_si128()); // 5, 0, 6, 0, 7, 0, 8, 0
+
+                    ulong packed_sum[16];
 
                     if (y == 0) {
-                        _mm512_inserti64x4((__m512i)*integralImage, srcSum, index.m512i_i32[0]);
-                        _mm512_inserti64x4((__m512i)*integralImage, srcSum, index.m512i_i32[4]);
-                        _mm512_inserti64x4((__m512i)*integralImage, srcSum, index.m512i_i32[8]);
-                        _mm512_inserti64x4((__m512i)*integralImage, srcSum, index.m512i_i32[12]);
+                        // Записываем в массив по два 32-битных значения, упакованных в ulong
+                        _mm_storel_epi64((__m128i*) (integralImage + 0), lo_64_01);
+                        _mm_storel_epi64((__m128i*) (integralImage + 1), hi_64_01);
+                        _mm_storel_epi64((__m128i*) (integralImage + 2), lo_64_23);
+                        _mm_storel_epi64((__m128i*) (integralImage + 3), hi_64_23);
                     }
                     else {
-                      /*  _mm512_inserti64x4((__m512i)*integralImage, srcSum, index.m512i_i32[0]);
-                        _mm512_inserti64x4((__m512i)*integralImage, srcSum, index.m512i_i32[4]);
-                        _mm512_inserti64x4((__m512i)*integralImage, srcSum, index.m512i_i32[8]);
-                        _mm512_inserti64x4((__m512i)*integralImage, srcSum, index.m512i_i32[12]);*/
+                       /* _mm_storel_epi64((__m128i*)(packed_sum), lo_64_01);
+                        _mm_storel_epi64((__m128i*)(packed_sum + 1), hi_64_01);
+                        _mm_storel_epi64((__m128i*)(packed_sum + 2), lo_64_23);
+                        _mm_storel_epi64((__m128i*)(packed_sum + 3), hi_64_23);*/
+
+                        for (int32 i = 0; i < 16; ++i) {
+
+                            int32 indx = index.m512i_i32[i];
+                            suma += src[indx];
+
+                            integralImage[indx] = integralImage[indx - 1] + suma;
+                        }
                     }
+                }
+            }
+
+
+            for (; x < width; ++x) {
+                long sum = 0;
+                for (; y < height; ++y) {
+                    int32 index = y * width + x;
+
+
+                    sum += src[index];
+
+                    if (y == 0)
+                        integralImage[index] = sum;
+                    else
+                        integralImage[index] = integralImage[index - 1] + sum;
+                }
+            }
+
+            for (int32 x = 0; x < width; ++x) {
+                for (int32 y = 0; y < height; ++y) {
+                    int32 index = y * width + x;
+
+                    int32 rectLeftX = x - SHalf;
+                    int32 rectRightX = x + SHalf;
+
+                    int32 rectTopY = y - SHalf;
+                    int32 rectBottomY = y + SHalf;
+
+                    if (rectLeftX < 0)
+                        rectLeftX = 0;
+
+                    if (rectRightX >= width)
+                        rectRightX = width - 1;
+
+                    if (rectTopY < 0)
+                        rectTopY = 0;
+
+                    if (rectBottomY >= height)
+                        rectBottomY = height - 1;
+
+                    int32 count = (rectRightX - rectLeftX) * (rectBottomY - rectTopY);
+
+                    long sum = integralImage[rectBottomY * width + rectRightX]
+                        - integralImage[rectTopY * width + rectRightX]
+                        - integralImage[rectBottomY * width + rectLeftX]
+                        + integralImage[rectTopY * width + rectLeftX];
+
+                    if ((long)(src[index] * count) < (long)(sum * (1.0 - _T)))
+                        res[index] = 0;
+                    else
+                        res[index] = 255;
                 }
             }
 
