4a47589aae7937a6f8613676987c88c5f0ee0e41
Midoritotoro <danya20090504@gmail.com>
2025-06-08 01:21:23 +0300

    Fix

--- tests/BaseMemcpyTest.cpp
+++ tests/BaseMemcpyTest.cpp
@@ -1,6 +1,7 @@
 #include <src/core/memory/crt/BaseMemcpy.h>
+#include <src/core/memory/crt/BaseMemmove.h>
 
 int main(int argc, char** argv) {
-	
+	std::cout << /*base::memory::*/__base_memmove();
 	return 0;
 }
