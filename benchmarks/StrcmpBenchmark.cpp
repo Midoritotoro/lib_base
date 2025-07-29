34186c5e3773a750f60efaa0d0f946b1fea1bc0f
Midoritotoro <danya20090504@gmail.com>
2025-07-29 20:09:03 +0300

    Fix

--- benchmarks/StrcmpBenchmark.cpp
+++ benchmarks/StrcmpBenchmark.cpp
@@ -9,26 +9,17 @@
 template <
     typename _Char_,
     StringAlignedSizeForBenchmark stringAlignedSizeForBenchmark = StringAlignedSizeForBenchmark::Large>
-class CRTStrlenBenchmark {
-    static auto StrcmpHelper(
-        const _Char_* const string1,
-        const _Char_* const string2)
-    {
-        if constexpr (std::is_same_v<_Char_, char>)
-            return strcmp(string1, string2);
-        else if constexpr (std::is_same_v<_Char_, wchar_t>)
-            return wcscmp(string1, string2);
-    }
-
+class CRTStrcmpBenchmark {
 public:
-    static auto Strcmp(benchmark::State& state) {
+    static void Strcmp(benchmark::State& state) {
         static constexpr auto textArray = FixedArray<_Char_, stringAlignedSizeForBenchmark>{};
         static constexpr auto textReversedArray = FixedReversedArray<_Char_, stringAlignedSizeForBenchmark>{};
 
-        for (auto _ : state) {
-            benchmark::DoNotOptimize(
-                StrcmpHelper(textArray.data, textReversedArray.data)
-            );
+        while (state.KeepRunning()) {
+            if constexpr (std::is_same_v<_Char_, char>)
+                strcmp(textArray.data, textReversedArray.data);
+            else if constexpr (std::is_same_v<_Char_, wchar_t>)
+                wcscmp(textArray.data, textReversedArray.data);
         }
     }
 };
@@ -36,85 +27,22 @@ public:
 template <
     typename _Char_,
     StringAlignedSizeForBenchmark stringAlignedSizeForBenchmark = StringAlignedSizeForBenchmark::Large>
-class StrlenBenchmark {
-    static auto StrcmpHelper(
-        const _Char_* const string1,
-        const _Char_* const string2)
-    {
-        return base::string::__base_any_strcmp(string1, string2);
-    }
-
+class StrcmpBenchmark {
 public:
     static auto Strcmp(benchmark::State& state) {
         static constexpr auto textArray = FixedArray<_Char_, stringAlignedSizeForBenchmark>{};
         static constexpr auto textReversedArray = FixedReversedArray<_Char_, stringAlignedSizeForBenchmark>{};
 
-        for (auto _ : state) {
-            benchmark::DoNotOptimize(
-                StrcmpHelper(textArray.data, textReversedArray.data)
-            );
-        }
+        while (state.KeepRunning())
+            base::string::__base_any_strcmp(textArray.data, textReversedArray.data);
     }
 };
 
-// ========================================================================================
 
-BASE_ADD_BENCHMARK(StrlenBenchmark<char, StringAlignedSizeForBenchmark::Small>::Strcmp);
-BASE_ADD_BENCHMARK(StrlenBenchmark<char, StringAlignedSizeForBenchmark::Medium>::Strcmp);
-BASE_ADD_BENCHMARK(StrlenBenchmark<char, StringAlignedSizeForBenchmark::Large>::Strcmp);
-
-// ========================================================================================
-
-BASE_ADD_BENCHMARK(CRTStrlenBenchmark<char, StringAlignedSizeForBenchmark::Small>::Strcmp);
-BASE_ADD_BENCHMARK(CRTStrlenBenchmark<char, StringAlignedSizeForBenchmark::Medium>::Strcmp));
-BASE_ADD_BENCHMARK(CRTStrlenBenchmark<char, StringAlignedSizeForBenchmark::Large>::Strcmp);
+BASE_ADD_BENCHMARK(
+    BASE_ECHO(StrcmpBenchmark<char, StringAlignedSizeForBenchmark::Small>::Strcmp),
+    BASE_ECHO(CRTStrcmpBenchmark<char, StringAlignedSizeForBenchmark::Small>::Strcmp)
+);
 
 
-// ========================================================================================
-// ========================================================================================
-// ========================================================================================
-
-
-BASE_ADD_BENCHMARK(StrlenBenchmark<wchar_t, StringAlignedSizeForBenchmark::Small>::Strcmp);
-BASE_ADD_BENCHMARK(StrlenBenchmark<wchar_t, StringAlignedSizeForBenchmark::Medium>::Strcmp);
-BASE_ADD_BENCHMARK(StrlenBenchmark<wchar_t, StringAlignedSizeForBenchmark::Large>::Strcmp);
-
-// ========================================================================================
-
-BASE_ADD_BENCHMARK(CRTStrlenBenchmark<wchar_t, StringAlignedSizeForBenchmark::Small>::Strcmp);
-BASE_ADD_BENCHMARK(CRTStrlenBenchmark<wchar_t, StringAlignedSizeForBenchmark::Medium>::Strcmp);
-BASE_ADD_BENCHMARK(CRTStrlenBenchmark<wchar_t, StringAlignedSizeForBenchmark::Large>::Strcmp);
-
-
-// ========================================================================================
-// ========================================================================================
-// ========================================================================================
-
-
-BASE_ADD_BENCHMARK(StrlenBenchmark<char32_t, StringAlignedSizeForBenchmark::Small>::Strcmp);
-BASE_ADD_BENCHMARK(StrlenBenchmark<char32_t, StringAlignedSizeForBenchmark::Medium>::Strcmp);
-BASE_ADD_BENCHMARK(StrlenBenchmark<char32_t, StringAlignedSizeForBenchmark::Large>::Strcmp);
-
-// ========================================================================================
-
-
-int main(int argc, char** argv) {
-    ::benchmark::MaybeReenterWithoutASLR(argc, argv);
-    char arg0_default[] = "benchmark";
-
-    char* args_default = reinterpret_cast<char*>(arg0_default);
-
-    if (!argv) {
-        argc = 1;
-        argv = &args_default;
-    }
-
-    ::benchmark::Initialize(&argc, argv);
-    if (::benchmark::ReportUnrecognizedArguments(argc, argv))
-        return 1;
-
-    ::benchmark::RunSpecifiedBenchmarks();
-    ::benchmark::Shutdown();
-
-    return 0;
-}
+BASE_BENCHMARK_MAIN();
